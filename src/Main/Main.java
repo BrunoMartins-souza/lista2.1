4d8fe688c0cd23f5c5ac9ed896b7c60cb9533505
Bruno Martins <brunodefatimoms@gmail.com>
2024-09-25 22:46:08 -0300

    Resolvendo exercicio 01 da lista 02.1

---
+++ src/Main/Main.java
@@ -0,0 +1,39 @@
+package Main;
+
+import java.util.Scanner;
+
+import package01.Exercicio01;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+
+        Integer opc = 0;
+
+        do {
+            System.out.println("Escolha uma opção:");
+            System.out.println(" 1 - Resolução Exercício 01");
+            System.out.println(" 2 - Resolução Exercício 02");
+            System.out.println(" 3 - Resolução Exercício 03");
+            System.out.println(" 4 - Sair.");
+            System.out.println();
+            opc = input.nextInt();
+            System.out.println();
+
+            switch (opc) {
+                case 1:
+                    Exercicio01 ex01 = new Exercicio01();
+                    ex01.exercicio01();
+                    break;
+
+                default:
+                    break;
+            }
+
+            System.out.println();
+
+        } while (opc != 4);
+
+        input.close();
+    }
+}
---
+++ src/package01/Exercicio01.java
@@ -0,0 +1,42 @@
+package package01;
+
+import java.util.Scanner;
+
+public class Exercicio01 {
+    public void exercicio01(){
+        Scanner input = new Scanner(System.in);
+
+            int matricula;
+            Double nota;
+            int maiorMatricula01 = 0;
+            Double maiorNota01 = 0.0;
+            int maiorMatricula02 = 0;
+            Double maiorNota02 = 0.0;
+
+            for (int i = 0; i < 3; i++){
+                System.out.println("Informe a nota do aluno " + (i + 1) + ":");
+                    nota = input.nextDouble();
+                System.out.println("Informe o número da matricula do aluno " + (i + 1) + ":");
+                    matricula = input.nextInt();
+
+                if (nota > maiorNota01) {
+                    maiorNota02 = maiorNota01;
+                    maiorMatricula02 = maiorMatricula01;
+
+                    maiorNota01 = nota;
+                    maiorMatricula01 = matricula;
+
+                }else if (nota > maiorNota02) {
+                    maiorNota02 = nota;
+                    maiorMatricula02 = matricula;
+                }
+            }
+            System.out.println("Aluno com a maior nota:");
+            System.out.println("Matricula: " + maiorMatricula01 + " - Nota: " + maiorNota01);
+
+            System.out.println("Aulo com seguna maior nota:");
+            System.out.println("Matricula: " + maiorMatricula02 + " - Nota: " + maiorNota02);
+            
+        input.close();
+    }
+}
